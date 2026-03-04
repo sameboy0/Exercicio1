cf403835079fa85b6c4faf18468b564e4fb76a49
sameboy0 <ryanundertale@gmail.com>
2026-03-04 09:29:36 -0300

    Add files via upload

---
+++ Atividade 7.java
@@ -0,0 +1,17 @@
+//Ryan Dias de Souza
+
+public class Main {
+    public static void main(String[] args) {
+
+    boolean c1, c2, c3, c4;
+
+        c1=  ((120-30) == Math.pow(3,30));
+        System.out.println(c1);
+        c2= (!(((20%4)/2) ==1) || (9!=9));
+        System.out.println(c2);
+        c3 = ((5%2 ) >3);
+        System.out.println(c3);
+        c4 = ("a"=="A");
+        System.out.println(c4);
+    }
+}
