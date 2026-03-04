7297bc3e86822d501cfe1ba50c1c0d140e0c482b
sameboy0 <ryanundertale@gmail.com>
2026-03-04 09:03:47 -0300

    Add files via upload

---
+++ Atividade 6.java
@@ -0,0 +1,25 @@
+//Ryan Dias de Souza
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner ler = new Scanner(System.in)) {
+
+            double c1, c2, c3, c4;
+
+            c1 = (double) (20 - 15) / 2;
+
+            System.out.println("O resultado da (20-15)/2 é:  " + c1);
+
+            c2 = (double) Math.pow(2, (5/20))+ 30 / Math.pow(15,2);
+            System.out.println("O resultado de 2 ˆ (5/20) + 30 / (15 ˆ 2) é:  " + c2);
+
+            c3 = (double)  35 / (6 + 2);
+            System.out.println("O resultado de 35 / (6 + 2) é:  " + c3);
+
+            c4 = (double) 23 % 4;
+            System.out.println("O resultado de 23 / 4 é:  " + c4);
+
+        }
+    }
+}
