f43bed9f218a4c85fbc26a79d154e561ad1b0687
sameboy0 <ryanundertale@gmail.com>
2026-03-04 08:31:40 -0300

    Add files via upload

---
+++ Atividade 4.java
@@ -0,0 +1,28 @@
+//Ryan Dias de Souza
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+try (Scanner ler= new Scanner(System.in) ){
+
+    int num1, num2, num3, num4, resultado;
+
+    System.out.println("Digite o seu primeiro número ");
+    num1 = ler.nextInt();
+    System.out.println("Digite o seu segundo número ");
+    num2 = ler.nextInt();
+    System.out.println("Digite o seu terceiro número ");
+    num3 = ler.nextInt();
+    System.out.println("Digite o seu quarto número ");
+    num4 = ler.nextInt();
+
+    resultado = num1 + num2 + num3 + num4;
+
+
+    System.out.println("O soma dos seus números foram: " + resultado);
+
+
+
+
+        }
+    }
+}
